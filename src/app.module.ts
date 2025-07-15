4fe92f36a04364bd2bc15b0babd8bc01fc7a15ca
Omar sabry <omarsabry@gmail.com>
2025-07-15 17:39:13 +0300

    Add Redis Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
+import { RedisModule } from './redis/redis.module';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [RedisModule],
   controllers: [AppController],
   providers: [AppService],
 })
